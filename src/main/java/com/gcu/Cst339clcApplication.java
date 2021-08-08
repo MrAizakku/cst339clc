ba1fb6857891b9457f738c04f608a7bf43fcefcd
Isaac Tucker <isaac.takka@gmail.com>
2021-08-08 14:30:15 -0700

    Sprint Boot Starter Project with SpringBoot Dev Tools, Thymeleaf and Spring Web
    Sprint Boot Starter Project with SpringBoot Dev Tools, Thymeleaf and Spring Web

---
+++ src/main/java/com/gcu/Cst339clcApplication.java
@@ -0,0 +1,13 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst339clcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst339clcApplication.class, args);
+	}
+
+}
