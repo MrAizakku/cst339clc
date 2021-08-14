a7d2f00dd53632675e4ad2d05d129c58debda60e
Isaac Tucker <isaac.takka@gmail.com>
2021-08-14 15:22:16 -0700

    pre-pending comments

--- src/main/java/com/gcu/controllers/HomeController.java
+++ src/main/java/com/gcu/controllers/HomeController.java
@@ -1,3 +1,23 @@
+/**
+ * ---------------------------------------------------------------------------
+ * Name      : Group Purple
+ * Members   : D. Johnson, I Tucker, I. Debenedetto, K. Kubli, K. Lamb
+ * Date      : 2021-08-14
+ * Class     : CST-339 Computer Programming III
+ * Professor : Brandon Bass
+ * Assignment: Milestone - CLC Group Assignment
+ * Disclaimer: This is our own work
+ * ---------------------------------------------------------------------------
+ * Description:
+ * 1. This is the home controller that specifies paths to direct a user to the index/home page.
+ * ---------------------------------------------------------------------------
+ * Modification History:
+ * Date     Name                Comment
+ * -------- ------------------- ----------------------------------------------
+ * 08/13/21 I. Tucker           Initial Creation
+ *
+ *
+ */
 package com.gcu.controllers;
 
 import org.springframework.stereotype.Controller;
