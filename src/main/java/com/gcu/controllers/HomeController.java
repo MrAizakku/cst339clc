520228e1afe4b652704418133280e16d83d6dc6f
Isaac Tucker <isaac.takka@gmail.com>
2021-08-15 02:09:26 -0700

    home controller edit

--- src/main/java/com/gcu/controllers/HomeController.java
+++ src/main/java/com/gcu/controllers/HomeController.java
@@ -24,12 +24,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class HomeController {
 
 	@GetMapping("")
 	public String home() {
 		//model.addAttribute("title","BLOG");
+		return "index";
+	}		
+
+	@GetMapping("/")
+	public String home_slash() {
 		return "index";
 	}	
 }
