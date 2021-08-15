25e83c36014f5a0dfc429295adbafecc884a99c4
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2021-08-15 00:31:48 -0700

    add login page & user model method
    adds login page and a get password method to the user model class.
    known bugs: form does not work to login... currently encounter error 404 upon submission

--- src/main/java/com/gcu/controllers/LoginController.java
+++ src/main/java/com/gcu/controllers/LoginController.java
@@ -1,5 +1,29 @@
 package com.gcu.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gcu.models.UserModel;
+
+@Controller
+@RequestMapping("/login")
 public class LoginController {
+
+	@GetMapping("/")
+	public String display(Model model) {
+		model.addAttribute(new UserModel());
+		return "login";
+	}
+
+	@PostMapping("/doLogin")
+	public String doLogin(UserModel userModel, BindingResult bindingResult, Model model) {
+		System.out.println(String.format("You entered username of %s and password of %s", userModel.getEmail(),
+				userModel.getPassword()));
+		return "login";
+	}
 
 }
