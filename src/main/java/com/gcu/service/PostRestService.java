3af65a3003c75f6d9083a2343f616012a797fdca
Isaac Tucker <isaac.takka@gmail.com>
2021-09-18 09:38:46 -0700

    added find post by ID

--- src/main/java/com/gcu/service/PostRestService.java
+++ src/main/java/com/gcu/service/PostRestService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,17 @@ public class PostRestService {
 	DataAccessInterface<PostModel> service;
 	
 	@GetMapping(path="/posts", produces= {MediaType.APPLICATION_JSON_VALUE})
-	public List<PostModel> getPOsts() {
+	public List<PostModel> getPosts() {
 		return service.findAll();
+	}
+	
+	@GetMapping(path="/posts/{string_id}", produces= {MediaType.APPLICATION_JSON_VALUE})
+	public PostModel getPostsById(@PathVariable("string_id") String sid) {
+	  try {
+		  int id = Integer.parseInt(sid);
+		  return service.findById(id);
+	  } catch (NumberFormatException e) {
+		  return null;
+	  }
 	}
 }
